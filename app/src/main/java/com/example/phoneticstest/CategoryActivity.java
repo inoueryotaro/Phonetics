b310334bb9a96b656cb80961ba0becb6e45b024d
ryochin0114 <ryochin@kwansei.ac.jp>
2019-10-27 08:57:12 +0900

    CategpryAcitivityでどのカテゴリーのボタンを押そうが TestActivityに遷移するようにした

--- app/src/main/java/com/example/phoneticstest/CategoryActivity.java
+++ app/src/main/java/com/example/phoneticstest/CategoryActivity.java
@@ -13,7 +13,252 @@ public class CategoryActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_category);
         Button Category_1Button = findViewById(R.id.category_1);
+        Button Category_2Button = findViewById(R.id.category_2);
+        Button Category_3Button = findViewById(R.id.category_3);
+        Button Category_4Button = findViewById(R.id.category_4);
+        Button Category_5Button = findViewById(R.id.category_5);
+        Button Category_6Button = findViewById(R.id.category_6);
+        Button Category_7Button = findViewById(R.id.category_7);
+        Button Category_8Button = findViewById(R.id.category_8);
+        Button Category_9Button = findViewById(R.id.category_9);
+        Button Category_10Button = findViewById(R.id.category_10);
+        Button Category_11Button = findViewById(R.id.category_11);
+        Button Category_12Button = findViewById(R.id.category_12);
+        Button Category_13Button = findViewById(R.id.category_13);
+        Button Category_14Button = findViewById(R.id.category_14);
+        Button Category_15Button = findViewById(R.id.category_15);
+        Button Category_16Button = findViewById(R.id.category_16);
+        Button Category_17Button = findViewById(R.id.category_17);
+        Button Category_18Button = findViewById(R.id.category_18);
+        Button Category_19Button = findViewById(R.id.category_19);
+        Button Category_20Button = findViewById(R.id.category_20);
+        Button Category_21Button = findViewById(R.id.category_21);
+        Button Category_22Button = findViewById(R.id.category_22);
+        Button Category_23Button = findViewById(R.id.category_23);
+
         Category_1Button.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View v) {
+                function();
+            }
+
+            private void function() {
+                Intent intent1 = new Intent(getApplication(), TestActivity.class);
+                startActivity(intent1);
+            }
+        });
+        Category_2Button.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View v) {
+                function();
+            }
+
+            private void function() {
+                Intent intent1 = new Intent(getApplication(), TestActivity.class);
+                startActivity(intent1);
+            }
+        });
+        Category_3Button.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View v) {
+                function();
+            }
+
+            private void function() {
+                Intent intent1 = new Intent(getApplication(), TestActivity.class);
+                startActivity(intent1);
+            }
+        }); Category_4Button.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View v) {
+                function();
+            }
+
+            private void function() {
+                Intent intent1 = new Intent(getApplication(), TestActivity.class);
+                startActivity(intent1);
+            }
+        }); Category_5Button.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View v) {
+                function();
+            }
+
+            private void function() {
+                Intent intent1 = new Intent(getApplication(), TestActivity.class);
+                startActivity(intent1);
+            }
+        }); Category_6Button.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View v) {
+                function();
+            }
+
+            private void function() {
+                Intent intent1 = new Intent(getApplication(), TestActivity.class);
+                startActivity(intent1);
+            }
+        }); Category_7Button.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View v) {
+                function();
+            }
+
+            private void function() {
+                Intent intent1 = new Intent(getApplication(), TestActivity.class);
+                startActivity(intent1);
+            }
+        }); Category_8Button.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View v) {
+                function();
+            }
+
+            private void function() {
+                Intent intent1 = new Intent(getApplication(), TestActivity.class);
+                startActivity(intent1);
+            }
+        }); Category_9Button.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View v) {
+                function();
+            }
+
+            private void function() {
+                Intent intent1 = new Intent(getApplication(), TestActivity.class);
+                startActivity(intent1);
+            }
+        }); Category_10Button.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View v) {
+                function();
+            }
+
+            private void function() {
+                Intent intent1 = new Intent(getApplication(), TestActivity.class);
+                startActivity(intent1);
+            }
+        }); Category_11Button.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View v) {
+                function();
+            }
+
+            private void function() {
+                Intent intent1 = new Intent(getApplication(), TestActivity.class);
+                startActivity(intent1);
+            }
+        }); Category_12Button.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View v) {
+                function();
+            }
+
+            private void function() {
+                Intent intent1 = new Intent(getApplication(), TestActivity.class);
+                startActivity(intent1);
+            }
+        }); Category_13Button.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View v) {
+                function();
+            }
+
+            private void function() {
+                Intent intent1 = new Intent(getApplication(), TestActivity.class);
+                startActivity(intent1);
+            }
+        }); Category_14Button.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View v) {
+                function();
+            }
+
+            private void function() {
+                Intent intent1 = new Intent(getApplication(), TestActivity.class);
+                startActivity(intent1);
+            }
+        }); Category_15Button.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View v) {
+                function();
+            }
+
+            private void function() {
+                Intent intent1 = new Intent(getApplication(), TestActivity.class);
+                startActivity(intent1);
+            }
+        }); Category_16Button.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View v) {
+                function();
+            }
+
+            private void function() {
+                Intent intent1 = new Intent(getApplication(), TestActivity.class);
+                startActivity(intent1);
+            }
+        }); Category_17Button.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View v) {
+                function();
+            }
+
+            private void function() {
+                Intent intent1 = new Intent(getApplication(), TestActivity.class);
+                startActivity(intent1);
+            }
+        }); Category_18Button.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View v) {
+                function();
+            }
+
+            private void function() {
+                Intent intent1 = new Intent(getApplication(), TestActivity.class);
+                startActivity(intent1);
+            }
+        }); Category_19Button.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View v) {
+                function();
+            }
+
+            private void function() {
+                Intent intent1 = new Intent(getApplication(), TestActivity.class);
+                startActivity(intent1);
+            }
+        }); Category_20Button.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View v) {
+                function();
+            }
+
+            private void function() {
+                Intent intent1 = new Intent(getApplication(), TestActivity.class);
+                startActivity(intent1);
+            }
+        }); Category_21Button.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View v) {
+                function();
+            }
+
+            private void function() {
+                Intent intent1 = new Intent(getApplication(), TestActivity.class);
+                startActivity(intent1);
+            }
+        }); Category_22Button.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View v) {
+                function();
+            }
+
+            private void function() {
+                Intent intent1 = new Intent(getApplication(), TestActivity.class);
+                startActivity(intent1);
+            }
+        }); Category_23Button.setOnClickListener(new View.OnClickListener() {
 
             public void onClick(View v) {
                 function();
